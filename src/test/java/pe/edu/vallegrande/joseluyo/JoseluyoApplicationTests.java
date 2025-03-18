285c83d05e79aede2652afd6cd9cb9837daa1ada
Jose Alberto Luyo Huari <jose.luyo.huari@vallegrande.edu.pe>
2025-03-18 14:04:29 +0000

    Spring Boot

---
+++ src/test/java/pe/edu/vallegrande/joseluyo/JoseluyoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.joseluyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoseluyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
